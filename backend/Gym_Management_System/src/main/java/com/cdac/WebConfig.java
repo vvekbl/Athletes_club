213376d4cc0cd238354187f495ac8baf165d51f3
vivek <vivekb.londhe@gmail.com>
2026-08-25 01:43:44 +0530

    CORS url added for port 5173

--- backend/Gym_Management_System/src/main/java/com/cdac/WebConfig.java
+++ backend/Gym_Management_System/src/main/java/com/cdac/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")
-            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedOrigins("http://localhost:3000", "http://localhost:5173")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
             .maxAge(3600);
